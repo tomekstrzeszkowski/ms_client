03c15050ab89640912f5a3b27b516e52f7e8dff1
tomek <tomasz@motoinsight.com>
2020-03-02 16:55:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="client", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Tomasz Strzeszkowski",
+    author_email="tomasz.Strzeszkowski@gmail.com.com",
+    description="Client wrapper for MarketScan",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/tomekstrzeszkowski/ms_client",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     install_requires=[
         'requests',
    ]
